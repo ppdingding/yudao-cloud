9052006096adcf6ff7465b43e0488eca22ea37b8
oliver <2545725692@qq.com>
2025-07-24 11:23:00 +0800

    [TEST]: test

---
+++ yudao-server/src/main/java/cn/iocoder/yudao/server/TestJenkins.java
@@ -0,0 +1,15 @@
+package cn.iocoder.yudao.server;
+
+public class TestJenkins {
+    public TestJenkins() {
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
